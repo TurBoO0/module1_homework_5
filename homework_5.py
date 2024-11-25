98ec632e971fc6f927cae58855bb10586be39f07
TurBoO0 <127961357+TurBoO0@users.noreply.github.com>
2024-11-25 15:41:21 +0300

    Add files via upload

---
+++ homework_5.py
@@ -0,0 +1,21 @@
+my_list = ["apple", 'orange', 'peach', 'melon', 'mango']
+print (f'List:{my_list}')
+print(f'First & last elements: {my_list[0:5:4]}')
+print(f'Sublist: {my_list[2:6]}')
+my_list[2] = 'redbomb'
+print(f'Modified list: {my_list}')
+
+my_dict = {
+    "white" : 'белый',
+    'green' : 'зеленый',
+    'yellow' : 'желтый',
+    'blue' : 'синий',
+    'red' : 'красный',
+}
+print(f'Dictionary: {my_dict}')
+change_value = my_dict.values()
+print(f'Translation: {my_dict['green']}')
+my_dict['blue'] = 'голубой'
+add_key = my_dict.keys()
+my_dict['black'] = 'черный'
+print(f'Modified dictionary: {my_dict}')
